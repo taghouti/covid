dc125594dc12213d71377e08e7d4aa4d810fc914
Taghouti Tarek <taghoutitarek@gmail.com>
2021-05-15 18:23:06 +0100

    api functions

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,68 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+import {Cases} from '../models/cases';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+const apiUrl = '/api/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  constructor(private http: HttpClient) {
+    this.http = http;
+  }
+
+  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+  getCases(): Observable<Cases[]> {
+    return this.http.get<Cases[]>(`${apiUrl}`)
+      .pipe(
+        tap(cases => console.log('fetched cases')),
+        catchError(this.handleError('getCases', []))
+      );
+  }
+
+  getCasesById(id: string): Observable<Cases> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get<Cases>(url).pipe(
+      tap(_ => console.log(`fetched cases id=${id}`)),
+      catchError(this.handleError<Cases>(`getCasesById id=${id}`))
+    );
+  }
+
+  addCases(cases: Cases): Observable<Cases> {
+    return this.http.post<Cases>(apiUrl, cases, httpOptions).pipe(
+      tap((c: Cases) => console.log(`added cases w/ id=${c.id}`)),
+      catchError(this.handleError<Cases>('addCases'))
+    );
+  }
+
+  updateCases(id: string, cases: Cases): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, cases, httpOptions).pipe(
+      tap(_ => console.log(`updated cases id=${id}`)),
+      catchError(this.handleError<any>('updateCases'))
+    );
+  }
+
+  deleteCases(id: string): Observable<Cases> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.delete<Cases>(url, httpOptions).pipe(
+      tap(_ => console.log(`deleted cases id=${id}`)),
+      catchError(this.handleError<Cases>('deleteCases'))
+    );
+  }
+}
