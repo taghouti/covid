c5b48969c81baec0dec60180436cc629a46cf1c5
Taghouti Tarek <taghoutitarek@gmail.com>
2021-05-15 19:27:48 +0100

    get case details

--- src/app/cases-details/cases-details.component.ts
+++ src/app/cases-details/cases-details.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {ActivatedRoute, Router} from '@angular/router';
+import {ApiService} from '../services/api.service';
+import {Cases} from '../models/cases';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-cases-details',
   templateUrl: './cases-details.component.html',
-  styleUrls: ['./cases-details.component.scss']
+  styleUrls: ['./cases-details.component.scss'],
+  providers: [DatePipe]
 })
 export class CasesDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentDate = new Date();
+
+  cases: Cases = { id: 0, name: '', gender: '', age: 0, address: '', city: '', country: '', status: '', updated: this.currentDate };
+  isLoadingResults = true;
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) {
+    this.route = route;
+    this.api = api;
+    this.router = router;
+  }
+
+  getCasesDetails(id: string): void {
+    this.api.getCasesById(id)
+      .subscribe((data: any) => {
+        this.cases = data;
+        console.log(this.cases);
+        this.isLoadingResults = false;
+      });
+  }
 
   ngOnInit(): void {
+    this.getCasesDetails(this.route.snapshot.params.id);
   }
 
 }
