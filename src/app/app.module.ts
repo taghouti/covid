7efa4342df1364cd36da56a30fe5a17f7e6519c3
Taghouti Tarek <taghoutitarek@gmail.com>
2021-05-15 17:49:52 +0100

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CasesComponent } from './cases/cases.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasesComponent
   ],
   imports: [
     BrowserModule,
