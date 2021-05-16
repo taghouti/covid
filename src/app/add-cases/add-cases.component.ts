29cad52c0abc3dca36fc690daf35998927d0cd06
Taghouti Tarek <taghoutitarek@gmail.com>
2021-05-16 01:11:05 +0100

    create cases

--- src/app/add-cases/add-cases.component.ts
+++ src/app/add-cases/add-cases.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ApiService} from '../services/api.service';
+import {FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-add-cases',
@@ -7,9 +18,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCasesComponent implements OnInit {
 
-  constructor() { }
+  casesForm!: FormGroup;
+  name = '';
+  gender = '';
+  age = 0;
+  address = '';
+  city = '';
+  country = '';
+  status = '';
+  statusList = ['Positive', 'Dead', 'Recovered'];
+  genderList = ['Male', 'Female'];
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
+    this.router = router;
+    this.api = api;
+    this.formBuilder = formBuilder;
+  }
 
   ngOnInit(): void {
+    this.casesForm = this.formBuilder.group({
+      name : [null, Validators.required],
+      gender : [null, Validators.required],
+      age : [null, Validators.required],
+      address : [null, Validators.required],
+      city : [null, Validators.required],
+      country : [null, Validators.required],
+      status : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit(): void {
+    this.isLoadingResults = true;
+    this.api.addCases(this.casesForm.value)
+      .subscribe((res: any) => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/cases-details', res.id]).then(r => console.log(r));
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
